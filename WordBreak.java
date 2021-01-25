ab94a081a85de93db5ee6f8672946f5db68bc64e
Todd Erickson <twericks@usc.edu>
2021-01-24 18:43:56 -0700

    Wordbreak with hashset memoization

--- WordBreak.java
+++ WordBreak.java
@@ -23,13 +23,19 @@ public class WordBreak {
       dict = Arrays.asList("car","ca","rs");
       canSplit = solve(s, dict);
       System.out.println(canSplit);
-   }
-   
-   public static boolean solve(String string, List<String> wordDict) {
-      return helper(0, string, wordDict);
+
+      s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
+      dict = Arrays.asList("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa");
+      canSplit = solve(s, dict);
+      System.out.println(canSplit);
    }
 
-   public static boolean helper(int index, String string, List<String> wordDict) {
+   public static boolean solve(String string, List<String> wordDict) {
+      HashSet<WordAtIndex> failures = new HashSet<>();
+      return helper(0, string, wordDict, failures);
+   }
+
+   public static boolean helper(int index, String string, List<String> wordDict, HashSet<WordAtIndex> failures) {
 
       if (index == string.length()) {
          return true;
@@ -38,12 +44,15 @@ public class WordBreak {
       String thisString = string.substring(index);
 
       for (String word : wordDict){
-         if (thisString.startsWith(word)){
+         WordAtIndex wordAtIndex = new WordAtIndex(word, index);
+         if (!failures.contains(wordAtIndex) && thisString.startsWith(word)){
             int wordLength = word.length();
             int newIndex = index + wordLength;
-            boolean recursive = helper(newIndex, string, wordDict);
-            if (recursive) {
+            boolean isSplitRecursive = helper(newIndex, string, wordDict, failures);
+            if (isSplitRecursive) {
                return true;
+            } else {
+               failures.add(wordAtIndex);
             }
          }
       }
@@ -51,4 +60,14 @@ public class WordBreak {
       return false;
    } 
 
+}
+
+class WordAtIndex {
+   private final String mWord;
+   private final int mIndex;
+
+   public WordAtIndex(String word, int index) {
+      mWord = word;
+      mIndex = index;
+   }
 }
