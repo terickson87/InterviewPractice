964f67adbaddaa39a144b292d976507d025e3c16
Todd Ericskon <todd.erickson87@gmail.com>
2023-10-22 10:40:41 -0700

    38/49 test cases passed

--- app/src/main/java/interviewpractice/NumIslands.java
+++ app/src/main/java/interviewpractice/NumIslands.java
@@ -1,8 +1,11 @@
 package interviewpractice;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class NumIslands {
     private static final char LAND = '1';
@@ -50,38 +53,97 @@ public class NumIslands {
         yMax = grid.length;
         xMax = grid[0].length;
         List<HashSet<Point>> islands = new ArrayList<HashSet<Point>>();
+        boolean[][] visited = new boolean[yMax][xMax]; //False default
+        Queue<Point> queue = new LinkedList<Point>();
+        Point firstPoint = new Point(0, 0);
+        queue.offer(firstPoint);
 
-        for (int i = 0; i < yMax; i++) {
-            for (int j = 0; j < xMax; j++) {
-                Point iPoint = new Point(j, i);
-                if (isLand(grid, iPoint)) {
-                    HashSet<Point> island = findExistingIsland(islands, iPoint, grid);
-                    if (island != null) {
-                        island.add(iPoint);
-                    } else {
-                        HashSet<Point> newIsland = new HashSet<>();
-                        newIsland.add(iPoint);
-                        islands.add(newIsland);
-                    }
+        while (!queue.isEmpty()) {
+            Point point = queue.poll();
+
+            if (visited[point.y][point.x] == true){
+                continue;
+            } else {
+                visited[point.y][point.x] = true;
+            }
+            addAdjacentPointsToQueue(visited, queue, point);
+
+            if (isLand(grid, point)) {
+                HashSet<Point> island = checkIfAdjacentPointsIsInAnExistingIsland(islands, point, grid);
+                if (island != null) {
+                    island.add(point);
+                } else {
+                    HashSet<Point> newIsland = new HashSet<>();
+                    newIsland.add(point);
+                    islands.add(newIsland);
                 }
             }
         }
 
+        boolean isChanged;
+        do {
+            isChanged = combineConnectedIslands(grid, islands);
+        } while (isChanged == true);
+
         return islands.size();
     }
 
-    // THis is a wrond implementation, need to do a BFS on every node
-    public HashSet<Point> findExistingIsland(List<HashSet<Point>> islands, Point point, char[][] grid) {
-        if (grid[point.y][point.x] == LAND) {
+    public boolean combineConnectedIslands(char[][] grid, List<HashSet<Point>> islands) {
+        for (HashSet<Point> island : islands) {
+            for (Point point : island) {
+                HashSet<Point> adjIsland = checkIfAdjacentPointsIsInAnExistingIsland(islands, point, grid);
+                if (null != adjIsland && adjIsland != island) {
+                    islands.remove(adjIsland);
+                    island.addAll(adjIsland);
+                    return true;
+                }
+            }    
+        }
+        return false;
+    }
+
+    public void addAdjacentPointsToQueue(boolean[][] visited, Queue<Point> queue, Point point) {
+        Point left = new Point(point.x-1,point.y);
+        addPointToQueueIfNotVisited(visited, queue, left);
+        Point right = new Point(point.x+1,point.y);
+        addPointToQueueIfNotVisited(visited, queue, right);
+        Point up = new Point(point.x,point.y+1);
+        addPointToQueueIfNotVisited(visited, queue, up);
+        Point down = new Point(point.x,point.y-1);
+        addPointToQueueIfNotVisited(visited, queue, down);
+    }
+
+    public void addPointToQueueIfNotVisited(boolean[][] visited, Queue<Point> queue, Point point) {
+        if (isInGrid(point) && visited[point.y][point.x] == false) {
+            queue.offer(point);
+        }
+    }
+
+    public HashSet<Point> checkIfAdjacentPointsIsInAnExistingIsland(List<HashSet<Point>> islands, Point point, char[][] grid) {
+        Point left = new Point(point.x-1,point.y);
+        Point right = new Point(point.x+1,point.y);
+        Point up = new Point(point.x,point.y+1);
+        Point down = new Point(point.x,point.y-1);
+        HashSet<Point> island;
+        island = findExistingIslandForPoint(islands, left, grid);
+        if (island != null)
+            return island;
+        island = findExistingIslandForPoint(islands, right, grid);
+        if (island != null)
+            return island;
+        island = findExistingIslandForPoint(islands, up, grid);
+        if (island != null)
+            return island;
+        island = findExistingIslandForPoint(islands, down, grid);
+        if (island != null)
+            return island;
+        return null;
+    }
+
+    public HashSet<Point> findExistingIslandForPoint(List<HashSet<Point>> islands, Point point, char[][] grid) {
+        if (isInGrid(point) && grid[point.y][point.x] == LAND) {
             for (HashSet<Point> island : islands) {
-                Point left = new Point(point.x-1,point.y);
-                Point right = new Point(point.x+1,point.y);
-                Point up = new Point(point.x,point.y+1);
-                Point down = new Point(point.x,point.y-1);
-                if (isInGrid(left) && island.contains(left) // contains is failing, test contains
-                || isInGrid(right) && island.contains(right)
-                || isInGrid(up) && island.contains(up)
-                || isInGrid(down) && island.contains(down))
+                if (island.contains(point))
                     return island;
             }
         }
