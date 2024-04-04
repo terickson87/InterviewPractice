988fbf749b08bfc85c27e14d642744def91f7799
Todd Ericskon <todd.erickson87@gmail.com>
2024-04-03 18:12:28 -0700

    Added CountingSubAnagrams tests

--- app/src/test/java/realinterviews/CountingSubAnagramsTest.java
+++ app/src/test/java/realinterviews/CountingSubAnagramsTest.java
@@ -27,5 +27,27 @@ public class CountingSubAnagramsTest {
 
         assertEquals(false, result);
     }
+
+    @Test
+    void given_bothEmpty_whenStringChallenge_returnsTrue() {
+        String str1 = "";
+        String str2 = "";
+
+        CountingSubAnagrams countingSubAnagrams = new CountingSubAnagrams();
+        boolean result = countingSubAnagrams.stringChallenge(str1, str2);
+
+        assertEquals(true, result);
+    }
+
+    @Test
+    void given_str2Empty_whenStringChallenge_returnsFalse() {
+        String str1 = "abc";
+        String str2 = "";
+
+        CountingSubAnagrams countingSubAnagrams = new CountingSubAnagrams();
+        boolean result = countingSubAnagrams.stringChallenge(str1, str2);
+
+        assertEquals(true, result);
+    }
     
 }
